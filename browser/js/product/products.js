71591a645b395e90b3850617f3d569a9bb08adc2
Elizabeth Funk <elizabeth_funk@hotmail.com>
2016-03-04 16:48:13 -0500

    products state and controller

--- browser/js/product/products.js
+++ browser/js/product/products.js
@@ -1,6 +1,19 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('products', {
         url: '/products',
-        templateUrl: 'js/product/products.html'
+        templateUrl: 'js/product/products.html',
+        controller: 'ProductsCtrl',
+
+        resolve: { 
+        	allProducts: function(ProductFactory){
+        		return ProductFactory.fetchAll();
+
+        	}
+    	}
     });
+});
+
+app.controller('ProductsCtrl', function($scope, allProducts) {
+	$scope.products = allProducts;
+	console.log("$scope.products: ", $scope.products);
 });
