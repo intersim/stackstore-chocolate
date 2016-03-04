3bd04a0b8ee1fac291342178af2c2fb71742e05f
Elizabeth Funk <elizabeth_funk@hotmail.com>
2016-03-04 15:56:06 -0500

    working on product factory

--- browser/js/product/product.factory.js
+++ browser/js/product/product.factory.js
@@ -5,24 +5,13 @@ app.factory('ProductFactory', function ($http) {
   var ProductFactory = {};
 
   ProductFactory.fetchAll = function () {
-    return $http.get('/api/albums')
+    return $http.get('/api/products')
     .then(response => response.data)
-    .then(albums => albums.map(ProductFactory.convert) );
   };
 
   ProductFactory.fetchById = function (id) {
-    return $http.get('/api/albums/' + id)
-    .then(response => response.data)
-    .then(ProductFactory.convert)
-    .then(album => {
-      album.songs = album.songs.map(SongFactory.convert);
-      return album;
-    });
-  };
-
-  ProductFactory.convert = function (album) {
-    album.imageUrl = '/api/albums/' + album._id + '.image';
-    return album;
+    return $http.get('/api/products/' + id)
+    .then(response => response.data);
   };
 
   return ProductFactory;
