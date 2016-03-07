d6af4b52ca02c9fd9ec361fbcc535af36533fbdc
Yuval Idan <yuval.idan5@gmail.com>
2016-03-07 17:49:22 -0500

    tried to add google login

--- server/env/development.js
+++ server/env/development.js
@@ -12,8 +12,8 @@ module.exports = {
     "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
   },
   "GOOGLE": {
-    "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
-    "clientSecret": "INSERT_GOOGLE_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_GOOGLE_CALLBACK_HERE"
+    "clientID": "1022144965124-7acplp6tv2p1lno54gop52fge06ui9f9.apps.googleusercontent.com",
+    "clientSecret": "vcguMJG5mj-WZzS0bpgmCEIF",
+    "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
   }
 };
