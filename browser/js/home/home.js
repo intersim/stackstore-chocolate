94270726bb2d0549ab06a555d3223c775cd6b4e7
Yuval Idan <yuval.idan5@gmail.com>
2016-03-08 22:30:35 -0500

    added homepage design

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,48 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeCtrl',
+        resolve: {
+        	theUser: function(AuthService) {
+        		return AuthService.getLoggedInUser()
+        	}
+        }
     });
 });
+
+
+
+app.controller('HomeCtrl', function($scope, theUser) {
+
+  $scope.user = theUser;
+
+  // $scope.isLoggedIn = function () {
+  //     return AuthService.isAuthenticated();
+  // };
+
+  // $scope.logout = function () {
+  //     AuthService.logout().then(function () {
+  //        $state.go('home');
+  //     });
+  // };
+
+  // var setUser = function () {
+  //     AuthService.getLoggedInUser().then(function (user) {
+  //         $scope.user = user;
+  //           console.log('scope.user', $scope.user)
+
+  //     });
+  // };
+
+  // var removeUser = function () {
+  //     $scope.user = null;
+  // };
+
+  // setUser();
+
+  // $rootScope.$on(AUTH_EVENTS.loginSuccess, setUser);
+  // $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
+  // $rootScope.$on(AUTH_EVENTS.sessionTimeout, removeUser);
+});
+
